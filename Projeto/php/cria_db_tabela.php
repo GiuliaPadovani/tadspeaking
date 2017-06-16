ae8f602c1e691ee841c20e6cfe77ca018a77bc54
Jorge Madson <jorgemadsonsv@gmail.com>
2017-06-16 19:58:41 -0300

    Todas as tabelas no cria_db_tabela

--- Projeto/php/cria_db_tabela.php
+++ Projeto/php/cria_db_tabela.php
@@ -24,34 +24,63 @@ if (mysqli_query($conn, $sql)) {
 }
 
 // sql to create table
-$sql = "CREATE TABLE Professor (
-  nome VARCHAR(255),
-  usuario VARCHAR(20),
-  senha VARCHAR(20),
-  email VARCHAR(255),
-  registro INTEGER AUTO_INCREMENT PRIMARY KEY,
-  dataRegistro VARCHAR(50),
-  administrador Boolean,
-  dataCriacao VARCHAR(20),
-  instituicao VARCHAR(50)
-);
-CREATE TABLE Questao (
-idQuestao INTEGER AUTO_INCREMENT PRIMARY KEY,
-acertos INTEGER,
-erros INTEGER,
-enunciado VARCHAR(255),
-resposta INTEGER,
-alter_a VARCHAR(255),
-alter_b VARCHAR(255),
-alter_c VARCHAR(255),
-alter_d VARCHAR(255),
-FOREIGN KEY(registro) REFERENCES Professor (registro)
-);
-CREATE TABLE Assunto (
-nome VARCHAR(255) PRIMARY KEY,
-qntdProvas INTEGER,
-qntdQuestoes INTEGER,
-)";
+$sql = 
+" 
+  CREATE TABLE Professor (
+    nome VARCHAR(255),
+    usuario VARCHAR(20),
+    senha VARCHAR(20),
+    email VARCHAR(255),
+    registro INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dataRegistro VARCHAR(50),
+    administrador Boolean,
+    dataCriacao VARCHAR(20),
+    instituicao VARCHAR(50)
+  );
+
+  CREATE TABLE Curso (
+    nome VARCHAR(255),
+    id_curso INTEGER PRIMARY KEY AUTO_INCREMENT
+  );
+
+  CREATE TABLE Disciplina (
+    nome_disciplina VARCHAR(255),
+    id_disciplina INTEGER PRIMARY KEY AUTO_INCREMENT
+  );
+
+  CREATE TABLE curso_disciplina (
+    id_disciplina INTEGER,
+    id_curso INTEGER,
+    FOREIGN KEY id_curso REFERENCES Curso(id_curso),
+    FOREIGN KEY id_disciplina REFERENCES Disciplina(id_disciplina),
+    PRIMARY KEY (id_disciplina, id_curso) 
+  );
+
+  CREATE TABLE Assunto (
+    nome_assunto VARCHAR(255), 
+    id_assunto INTEGER PRIMARY KEY,
+    qntdQuestoes INTEGER,
+    id_disciplina INTEGER,
+    FOREIGN KEY (id_disciplina) REFERENCES Disciplina (id_disciplina)
+  );
+
+  CREATE TABLE Questao (
+    id_questao INTEGER AUTO_INCREMENT PRIMARY KEY,
+    acertos INTEGER,
+    erros INTEGER,
+    enunciado VARCHAR(255),
+    resposta CHAR(1),
+    alter_a VARCHAR(255),
+    alter_b VARCHAR(255),
+    alter_c VARCHAR(255),
+    alter_d VARCHAR(255),
+    registro INTEGER,
+    id_assunto INTEGER,
+    FOREIGN KEY(registro) REFERENCES Professor (registro),
+    FOREIGN KEY (id_assunto) REFERENCES Assunto (id_assunto)
+  );";
+
+  echo $sql;
 
 if (mysqli_query($conn, $sql)) {
     echo "<br>Database created successfully";
