779e25e78415bb3074c2c02bb4e9f2c35cbe6c0f
Jorge Madson <jorgemadsonsv@gmail.com>
2017-06-16 16:37:12 -0300

    Agora foi

---
+++ Projeto/php/cria_db_tabela.php
@@ -0,0 +1,47 @@
+<?php
+require 'lib/credentials.php';
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE $dbname";
+if (mysqli_query($conn, $sql)) {
+    echo "<br>Database created successfully<br>";
+} else {
+    echo "<br>Error creating database: " . mysqli_error($conn);
+}
+
+// Choose database
+$sql = "USE $dbname";
+if (mysqli_query($conn, $sql)) {
+    echo "<br>Database changed";
+} else {
+    echo "<br>Error creating database: " . mysqli_error($conn);
+}
+
+// sql to create table
+$sql = "CREATE TABLE Professor (
+  nome VARCHAR(255),
+  usuario VARCHAR(20),
+  senha VARCHAR(20),
+  email VARCHAR(255),
+  registro VARCHAR(20) PRIMARY KEY,
+  dataRegistro VARCHAR(50),
+  administrador Boolean,
+  dataCriacao VARCHAR(20),
+  instituicao VARCHAR(50)
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "<br>Database created successfully";
+} else {
+    echo "<br>Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
