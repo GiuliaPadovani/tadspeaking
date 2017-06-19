e8f323628fb61869b42303f4dcb6eeac6502eecb
Jorge Madson <jorgemadsonsv@gmail.com>
2017-06-19 16:03:27 -0300

    commit final

--- Projeto/html/lista.php
+++ Projeto/html/lista.php
@@ -1,14 +1,53 @@
 <?php
+	include "../php/select_exercicios.php";
 	require_once "../php/lib/credentials.php";
 	require_once "../php/lib/connection.php";
 	require_once "../php/authenticate.php";
+
+//------------recebe resposta da questao--------------
+	if ($_SERVER['REQUEST_METHOD']=="POST"){
+		if(isset($_POST['enviar_resposta'])){
+			$id_questao=$_POST['id_questao'];
+			$resp_usuario=$_POST['resp'];
+			echo $id_questao;
+			echo '<br>';
+			echo $resp_usuario;
+//----------fazendo select para verificar se está correto-----
+			$sql = "SELECT id_questao,resposta FROM Questao where id_questao='$id_questao';";
+		    $dados = mysqli_query($conn, $sql);
+		    if (!$dados) 
+		    {
+		        die('Problemas no select.');
+		    }else
+		    {
+		    	$dados_do_banco=mysqli_fetch_assoc($dados);
+		    
+		    	if($dados_do_banco>0)
+		    	{
+		    		
+		    		if ($id_questao == $dados_do_banco['id_questao'])
+		    		{
+		    			if($resp_usuario==$dados_do_banco['resposta'])
+		    			{
+		    				echo "<script> alert('Voce acertou');</script>";
+		    			}
+		    			else
+		    			{
+		    				echo "<script> alert('Voce errou');</script>";
+		    			}
+		    		}
+		    	}
+
+		    }	
+}
+}
 ?>
 <!DOCTYPE>
 <html>
 <head> 
     <title>TadSpeaking - Lista de Exercicio</title>
 	<meta charset="utf-8">
-	<script src="https://use.fontawesome.com/a67d3afb0f.js"></script> <!-- icones fofos -->
+	  <link rel="stylesheet" href="../css/font-awesome.min.css"><!-- icones fofos -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
@@ -29,97 +68,19 @@
 				</div>
 				<div class="mostraExercicios">
 					<div class="row">
+					<!--verificaRespostas.php-->
+					<form action="lista.php" method="POST">
 						<div class="col-md-12 ex">
 							<?php
-								global $user_id, $conn;
-function exibeExercicios($dados)
-
-{
-    if ($dados==NULL) {
-
-    	echo "<h4>Não existem exercícios para serem exibidos.</h4>";
-
-    }else{
-
-	    if (mysqli_num_rows($dados) > 0) {
-
-	        $i = 1;
-
-	        while ($dado = mysqli_fetch_assoc($dados)) {
-
-	        	$output="<div class='col-md-12 ex'>";
-
-	            $output.="<h4>".$i.".".$dado['enunciado']."</h4>";
-			$output.="<label><input type='checkbox' name='resposta' value='a'></label>";
-	            $output.="<p class='textoInline choice'>a)</p><p class='textoInline'>".$dado['alter_a']."</p><br>";
-			$output.="<label><input type='checkbox' name='resposta' value='b'></label>";
-	            $output.="<p class='textoInline choice'>b)</p><p class='textoInline'>".$dado['alter_b']."</p><br>";
-			$output.="<label><input type='checkbox' name='resposta' value='c'></label>";
-	            $output.="<p class='textoInline choice'>c)</p><p class='textoInline'>".$dado['alter_c']."</p><br>";
-			$output.="<label><input type='checkbox' name='resposta' value='d'></label>";
-	            $output.="<p class='textoInline choice'>d)</p><p class='textoInline'>".$dado['alter_d']."</p><br>";
-
-	            
-
-	            $sql = "SELECT nome FROM Professor WHERE registro='".$dado['registro']."';";
-
-	            
-
-	            $ret = mysqli_query($conn, $sql);
-
-	            
-
-	            if (!$ret) {
-
-	                die('Problemas no select.');
-
-	            } else {
-
-	                if (mysqli_num_rows($ret) > 0) {
-
-	                    $x = mysqli_fetch_assoc($ret);
-
-	                    $nome_autor = $x["nome"];
-
-	                }
-
-	          }
-
-	            $output.="<div class='col-md-4 author'><p><i>By ".$nome_autor."</i></p></div>";
-
-	            $output.="</div>
-
-							<div class='row'>
-
-								<div class='col-md-12 options'>
-
-									<span class='like'><i class='fa fa-thumbs-o-up icon like' aria-hidden='true'></i></span>
-
-									<span class='dislike'><i class='fa fa-thumbs-o-down icon' aria-hidden='true'></i></span>
-
-								</div>
-
-							</div>";
-
-				echo $output;
-
-	            
-
-	            $i++;
-
-	        }
-
-	    }
-
-	}
-}
-
-exibeExercicios("Select * from Questao;");
+								exibeExerciciosLista(selectTodosExercicios());					
 							?>
 					</div>
 				<div class="row">
 					<div class="col-md-8"></div>
-					<div class="col-md-4"></div>
+					<div class="col-md-4">
+						<input type="submit" name="enviar_resposta" value="Enviar" class="botao botaoVerde">
+					</div>
+					</form>
 				</div>
 			</div>			
 		</div>			
--- Projeto/php/select_exercicios.php
+++ Projeto/php/select_exercicios.php
@@ -54,6 +54,52 @@ function exibeExercicios($dados)
 	}
 }
 
+/*-------------------------Exibe Lista de Exercicio-------------------------------*/
+function exibeExerciciosLista($dados)
+{
+	global $user_id, $conn;
+    if ($dados==NULL) {
+    	echo "<h4>Não existem exercícios para serem exibidos.</h4>";
+    }else{
+	    if (mysqli_num_rows($dados) > 0) {
+	        $i = 1;
+	        while ($dado = mysqli_fetch_assoc($dados)) {
+		       	$output="<div class='col-md-12 ex'>";
+		        $output.="<h4>".$i.".".$dado['enunciado']."</h4>";
+				$output.="<label><input type='radio' name='resp' value='a'></label>";
+		        $output.="<p class='textoInline choice'>a)</p><p class='textoInline'>".$dado['alter_a']."</p><br>";
+				$output.="<label><input type='radio' name='resp' value='b'></label>";
+		        $output.="<p class='textoInline choice'>b)</p><p class='textoInline'>".$dado['alter_b']."</p><br>";
+				$output.="<label><input type='radio' name='resp' value='c'></label>";
+		        $output.="<p class='textoInline choice'>c)</p><p class='textoInline'>".$dado['alter_c']."</p><br>";
+				$output.="<label><input type='radio' name='resp' value='d'></label>";
+		        $output.="<p class='textoInline choice'>d)</p><p class='textoInline'>".$dado['alter_d']."</p><br>";
+		        $output.="<input name='id_questao' style='display: none' value='".$dado['id_questao']."'>";
+		        
+	            $sql = "SELECT nome FROM Professor WHERE registro='".$dado['registro']."';";
+	            $ret = mysqli_query($conn, $sql);
+	            if (!$ret) {
+	                die('Problemas no select.');
+	            } else {
+	                if (mysqli_num_rows($ret) > 0) {
+	                    $x = mysqli_fetch_assoc($ret);
+	                    $nome_autor = $x["nome"];
+	                }
+	          }
+	            $output.="<div class='col-md-4 author'><p><i>By ".$nome_autor."</i></p></div>";
+	            $output.="</div>
+							<div class='row'>
+								<div class='col-md-12 options'>
+									<span class='like'><i class='fa fa-thumbs-o-up icon like' aria-hidden='true'></i></span>
+									<span class='dislike'><i class='fa fa-thumbs-o-down icon' aria-hidden='true'></i></span>
+								</div>
+							</div>";
+				echo $output;
+	            $i++;
+	        }
+	    }
+	}
+}
 
 /*----------------------------Faz select dos exercícios---------------------------*/
 
